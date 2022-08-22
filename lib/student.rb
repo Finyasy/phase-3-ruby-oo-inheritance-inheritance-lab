04b2d3373f87fc6f166cc0d13ba4bc25cba90a82
Bryan Bosire <bryanbosire@gmail.com>
2022-08-22 13:57:21 +0300

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative './user'
 
+class Student < User
+    # Student.new is initialized with an empty @knowledge array
+    def initialize
+        @knowledge = []
+    end
+
+    # Student#learn takes in an argument of a string of knowledge and adds it to the student's knowledge array
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+    # Student#knowledge returns that student's knowledge array
+    def knowledge
+        @knowledge
+    end
 end
